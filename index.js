091f78246ec8a70e3c00ef9f9a6e53313f5357e7
Selenbp1 <akfkdhwk@gmail.com>
2022-02-26 20:18:43 +0900

    index.js, package-lock.json, package.json 수정

--- index.js
+++ index.js
@@ -2,14 +2,35 @@ const express = require('express')
 const app = express()
 const port = 6600
 
+const {User} = require("./models/User");
+const bodyParser = require('body-parser');
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://akfkdhwk:qudals090862@cluster0.syb7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! nodemon')
 })
+
+app.post('/register', (req,res) =>{
+    //회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다. 
+    
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
