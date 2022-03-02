30176507424c4673a2a99a88334356928a6a9270
Selenbp1 <akfkdhwk@gmail.com>
2022-03-02 19:34:26 +0900

    인증체크

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import {useDispatch} from 'react-redux';
 import {loginUser} from '../../../_actions/user_actions';
 import {useNavigate} from 'react-router-dom';
+import Auth from '../../../hoc/auth'
 
 function LoginPage(){
     const navigate = useNavigate();
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {useDispatch} from 'react-redux';
 import {registerUser} from '../../../_actions/user_actions';
 import {useNavigate} from 'react-router-dom';
+import Auth from '../../../hoc/auth'
 
 function RegisterPage(){
     const navigate = useNavigate();
